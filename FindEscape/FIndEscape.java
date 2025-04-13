f78d5c7ea0f9aa96eb01bb932dec3e6aacaf5dcc
dtaing11 <157168922+dtaing11@users.noreply.github.com>
2025-04-12 23:23:56 -0500

    Hello

---
+++ FindEscape/FIndEscape.java
@@ -0,0 +1,12 @@
+package FindEscape;
+
+public class FindEscape {
+    Symbol.Table escEnv = new Symbol.Table(); // escEnv maps Symbol to Escape
+
+    private Absyn.FunctionDeclaration func = null;
+
+    public FindEscape(Absyn.Statement s) {  (0, s);  }
+
+
+
+}
--- Semant/Semant.java
+++ Semant/Semant.java
@@ -24,9 +24,71 @@ public class Semant {
   static final Types.STRING STRING = new Types.STRING();
   static final Types.NIL    NIL    = new Types.NIL();
 
+
   private Exp checkInt(ExpTy et, int pos) {
     if (!INT.coerceTo(et.ty))
       error(pos, "integer required");
     return et.exp;
   }
+
+  private Exp checkComparable(ExpTy et, int pos) {
+    Type a = et.ty.actual();
+    if (!(a instanceof Types.INT
+            || a instanceof Types.STRING
+            || a instanceof Types.NIL
+            || a instanceof Types.RECORD
+            || a instanceof Types.ARRAY))
+      error(pos, "integer, string, nil, record or array required");
+    return et.exp;
+  }
+  ExpTy transExp(Absyn.Exp e) {
+    ExpTy result;
+
+    if (e == null)
+      return new ExpTy(translate.Error(), VOID);
+    else if (e instanceof Absyn.VarExp)
+      result = transExp((Absyn.VarExp)e);
+    else if (e instanceof Absyn.NilExp)
+      result = transExp((Absyn.NilExp)e);
+    else if (e instanceof Absyn.IntExp)
+      result = transExp((Absyn.IntExp)e);
+    else if (e instanceof Absyn.StringExp)
+      result = transExp((Absyn.StringExp)e);
+    else if (e instanceof Absyn.CallExp)
+      result = transExp((Absyn.CallExp)e);
+    else if (e instanceof Absyn.OpExp)
+      result = transExp((Absyn.OpExp)e);
+    else if (e instanceof Absyn.RecordExp)
+      result = transExp((Absyn.RecordExp)e);
+    else if (e instanceof Absyn.SeqExp)
+      result = transExp((Absyn.SeqExp)e);
+    else if (e instanceof Absyn.AssignExp)
+      result = transExp((Absyn.AssignExp)e);
+    else if (e instanceof Absyn.)
+      result = transExp((Absyn.IfElseStatement)e);
+    else if (e instanceof Absyn.WhileExp)
+      result = transExp((Absyn.WhileExp)e);
+    else if (e instanceof Absyn.ForExp)
+      result = transExp((Absyn.ForExp)e);
+    else if (e instanceof Absyn.BreakExp)
+      result = transExp((Absyn.BreakExp)e);
+    else if (e instanceof Absyn.LetExp)
+      result = transExp((Absyn.LetExp)e);
+    else if (e instanceof Absyn.ArrayExp)
+      result = transExp((Absyn.ArrayExp)e);
+    else throw new Error("Semant.transExp");
+    e.type = result.ty;
+    return result;
+  }
+
+  private Exp checkOrderable(ExpTy et, int pos) {
+    Type a = et.ty.actual();
+    if (!(a instanceof Types.INT
+            || a instanceof Types.STRING))
+      error(pos, "integer or string required");
+    return et.exp;
+  }
+
 }
+
+
